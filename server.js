b6384d69766750b0ce5e553334dd5ea1283c4354
LAPTOP-GB9H4FVO\Alejo <alejandroa88@gmail.com>
2021-08-03 18:40:34 -0800

    prueba servicio

--- server.js
+++ server.js
@@ -14,5 +14,20 @@ app.post('/', function(req, res) {
   });
 });
 
+app.get('/consultaDpi', function(req, res) {
+  var body;
+  body = {"id":"123"};
+  res.json( [body] )
+  console.log('Hello, I am foo.');
+});
+
+
+app.get('/entregaDPI', function(req, res) {
+  var body;
+  body = {"id":"123", "estado":true};
+  res.json( [body] )
+  console.log('Hello, I am foo.');
+});
+
 app.listen(port);
 module.exports = app;
