a72543bd56d7e2c3567fe135b3169de0dc81e813
LAPTOP-GB9H4FVO\Alejo <alejandroa88@gmail.com>
2021-08-03 19:01:04 -0800

    consumir servicio

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const request = require('request');
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
@@ -28,6 +29,21 @@ app.get('/entregaDPI', function(req, res) {
   res.json( [body] )
   console.log('Hello, I am foo.');
 });
+
+
+app.get('/grupo21', function(req, res) {
+  request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res2, body) => {
+  if (err) { return console.log(err); }
+    console.log(body.url);
+    console.log(body.explanation);
+    res.json(body.explanation);
+  });
+});
+
+
+
+
+
 
 app.listen(port);
 module.exports = app;
